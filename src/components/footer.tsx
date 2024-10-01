5592dea12c389aefa57880a034975d5d88958057
raphauy <rapha.uy@rapha.uy>
2024-10-01 12:33:19 -0300

    widget

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <footer className='py-4 bg-muted/40'>
-      <div className='px-4 flex flex-col items-center justify-between gap-x-3 gap-y-1 text-center text-sm text-muted-foreground sm:flex-row'>
+      <div className='px-20 flex flex-col items-center justify-between gap-x-3 gap-y-1 text-center text-sm text-muted-foreground sm:flex-row'>
         <p>
           Tinta &copy;{new Date().getFullYear()}.
         </p>
