ec8f250737cae5b0617b1900f7df455d53cd7de4
raphauy <rapha.uy@rapha.uy>
2024-10-02 08:45:27 -0300

    políticas

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <footer className='py-4 bg-muted/40'>
+    <footer className='py-4 pr-12 bg-muted/40'>
       <div className='px-4 sm:px-6 md:px-8 flex flex-col items-center justify-between gap-y-4 text-center text-sm text-muted-foreground sm:flex-row sm:gap-x-3'>
         <div className='flex flex-col sm:flex-row items-center gap-2 sm:gap-4'>
           <div className='flex flex-wrap justify-center gap-2 sm:gap-4'>
