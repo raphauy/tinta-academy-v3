8b11581a345da20e0d8108dc651c1f278c9c0fbe
raphauy <rapha.uy@rapha.uy>
2024-11-12 10:39:40 -0300

    metadata fix

--- src/app/cursos/[slug]/page.tsx
+++ src/app/cursos/[slug]/page.tsx
@@ -26,7 +26,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
   return {
     title: course.title,
-    description: course.description,
+    description,
     openGraph: {
       title: course.title,
       description,
